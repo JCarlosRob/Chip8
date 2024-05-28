008adfcf0452732188a8dee7b68c5ef9b3ac7037
Juan Carlos Robles <carlos.jcr@hotmail.com>
2024-05-28 23:50:45 +0200

    add jump address instruction

---
+++ src/main/java/com/chip8/model/core/instruction/JumpAddressInstruction.java
@@ -0,0 +1,35 @@
+package com.chip8.model.core.instruction;
+
+import com.chip8.api.core.instruction.Instruction;
+import com.chip8.model.core.register.ProgramCounterHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+import java.util.HexFormat;
+import java.util.Objects;
+
+@Component
+public class JumpAddressInstruction implements Instruction {
+
+    private static final Integer COMMAND = HexFormat.fromHexDigits("1");
+
+    private final ProgramCounterHandler pc;
+
+    public JumpAddressInstruction(final ProgramCounterHandler pc) {
+        this.pc = pc;
+    }
+
+    @Override
+    public Boolean isExecutable(final String data) {
+        Assert.notNull(data, "Data can not be null");
+        Assert.hasLength(data, "Data can not empty");
+        return Objects.equals(COMMAND, HexFormat.fromHexDigits(data.substring(0, 1)));
+    }
+
+    @Override
+    public void run(final String data) {
+        Assert.notNull(data, "Data can not be null");
+        Assert.hasLength(data, "Data can not empty");
+        this.pc.setPc((short) HexFormat.fromHexDigits(data.substring(1)));
+    }
+}
---
+++ src/test/java/com/chip8/model/core/instruction/JumpAddressInstructionTest.java
@@ -0,0 +1,59 @@
+package com.chip8.model.core.instruction;
+
+import com.chip8.model.core.register.ProgramCounterHandler;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HexFormat;
+
+@ExtendWith(MockitoExtension.class)
+class JumpAddressInstructionTest {
+
+    @Mock
+    private ProgramCounterHandler pc;
+
+    @InjectMocks
+    private JumpAddressInstruction jumpAddressInstruction;
+
+    @Test
+    void isExecutable_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.jumpAddressInstruction.isExecutable(null));
+    }
+
+    @Test
+    void isExecutable_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.jumpAddressInstruction.isExecutable(""));
+    }
+
+    @Test
+    void isExecutable_inputCommand_returnFalse_test() {
+        Assertions.assertFalse(this.jumpAddressInstruction.isExecutable("0000"));
+    }
+
+    @Test
+    void isExecutable_inputCommand_returnTrue_test() {
+        Assertions.assertTrue(this.jumpAddressInstruction.isExecutable("1FFF"));
+    }
+
+    @Test
+    void run_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.jumpAddressInstruction.run(null));
+    }
+
+    @Test
+    void run_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.jumpAddressInstruction.run(""));
+    }
+
+    @Test
+    void run_test() {
+        this.jumpAddressInstruction.run("1FFF");
+        Mockito.verify(this.pc, Mockito.times(1)).setPc((short) HexFormat.fromHexDigits("FFF"));
+    }
+
+}
