bc8ea575afbab924615358207109bca3a90fb9f6
Juan Carlos Robles <carlos.jcr@hotmail.com>
2024-05-29 23:30:26 +0200

    add loader romm handler

--- src/main/java/com/chip8/model/loader/LoaderRomHandler.java
+++ src/main/java/com/chip8/model/loader/LoaderRomHandler.java
@@ -2,21 +2,24 @@ package com.chip8.model.loader;
 
 import com.chip8.api.loader.LoaderRom;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.IntStream;
 
 @Component
 public class LoaderRomHandler implements LoaderRom {
 
-
     @Override
     public Byte[] load(final String path) throws IOException {
-        final Path pathRom = Paths.get("src/main/resources/roms/PONG");
-        final byte[] bytes = Files.readAllBytes(pathRom);
-
-        return new Byte[]{};
+        Assert.isTrue(StringUtils.hasLength(path), "The path can not be null or empty");
+        final Path pathRom = Paths.get(path);
+        final byte[] romBytes = Files.readAllBytes(pathRom);
+        return IntStream.range(0, romBytes.length).mapToObj(i -> romBytes[i]).toArray(Byte[]::new);
     }
+
 }
---
+++ src/test/java/com/chip8/model/loader/LoaderRomHandlerTest.java
@@ -0,0 +1,34 @@
+package com.chip8.model.loader;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+class LoaderRomHandlerTest {
+
+    private final LoaderRomHandler loaderRomHandler = new LoaderRomHandler();
+
+    @Test
+    void load_test() throws IOException {
+        final Byte[] result = this.loaderRomHandler.load("src/test/resources/roms/testFile");
+        final Byte[] expected = {116, 101, 115, 116};
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void load_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.loaderRomHandler.load(null));
+    }
+
+    @Test
+    void load_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.loaderRomHandler.load(""));
+    }
+
+    @Test
+    void load_inputPathNotExist_returnException_test() {
+        Assertions.assertThrows(IOException.class, () -> this.loaderRomHandler.load("/"));
+    }
+
+}
