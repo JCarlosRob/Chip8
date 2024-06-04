886b34128df46d65d4f8a18c865e78d56619ea97
Juan Carlos Robles <carlos.jcr@hotmail.com>
2024-06-04 21:34:20 +0200

    add add vector into vector with carry instruction

---
+++ src/test/java/com/chip8/model/core/instruction/AddVectorIntoVectorWithCarryInstructionTest.java
@@ -0,0 +1,83 @@
+package com.chip8.model.core.instruction;
+
+import com.chip8.api.core.register.VectorRegister;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HexFormat;
+
+@ExtendWith(MockitoExtension.class)
+class AddVectorIntoVectorWithCarryInstructionTest {
+
+    @Mock
+    private VectorRegister vectorXRegister;
+
+    @Mock
+    private VectorRegister vectorYRegister;
+
+    private AddVectorIntoVectorWithCarryInstruction addVectorIntoVectorWithCarryInstruction;
+
+    @BeforeEach
+    void setUp() {
+        this.addVectorIntoVectorWithCarryInstruction = new AddVectorIntoVectorWithCarryInstruction(this.vectorXRegister, this.vectorYRegister);
+    }
+
+    @Test
+    void isExecutable_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.addVectorIntoVectorWithCarryInstruction.isExecutable(null));
+    }
+
+    @Test
+    void isExecutable_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.addVectorIntoVectorWithCarryInstruction.isExecutable(""));
+    }
+
+    @Test
+    void isExecutable_inputCommand_returnFalse_test() {
+        Assertions.assertFalse(this.addVectorIntoVectorWithCarryInstruction.isExecutable("0000"));
+    }
+
+    @Test
+    void isExecutable_inputCommand_returnTrue_test() {
+        Assertions.assertTrue(this.addVectorIntoVectorWithCarryInstruction.isExecutable("8FF4"));
+    }
+
+    @Test
+    void run_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.addVectorIntoVectorWithCarryInstruction.run(null));
+    }
+
+    @Test
+    void run_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.addVectorIntoVectorWithCarryInstruction.run(""));
+    }
+
+    @Test
+    void run_withCarry_test() {
+        Mockito.when(this.vectorXRegister.getVRegister(HexFormat.fromHexDigits("A"))).thenReturn(255);
+        Mockito.when(this.vectorYRegister.getVRegister(HexFormat.fromHexDigits("F"))).thenReturn(1);
+        this.addVectorIntoVectorWithCarryInstruction.run("8AF2");
+        Mockito.verify(this.vectorXRegister, Mockito.times(1))
+                .setVRegister(HexFormat.fromHexDigits("A"), 0);
+        Mockito.verify(this.vectorXRegister, Mockito.times(1))
+                .setVRegister(HexFormat.fromHexDigits("F"), 1);
+    }
+
+    @Test
+    void run_withoutCarry_test() {
+        Mockito.when(this.vectorXRegister.getVRegister(HexFormat.fromHexDigits("A"))).thenReturn(254);
+        Mockito.when(this.vectorYRegister.getVRegister(HexFormat.fromHexDigits("F"))).thenReturn(1);
+        this.addVectorIntoVectorWithCarryInstruction.run("8AF2");
+        Mockito.verify(this.vectorXRegister, Mockito.times(1))
+                .setVRegister(HexFormat.fromHexDigits("A"), 255);
+        Mockito.verify(this.vectorXRegister, Mockito.times(1))
+                .setVRegister(HexFormat.fromHexDigits("F"), 0);
+    }
+
+
+}
