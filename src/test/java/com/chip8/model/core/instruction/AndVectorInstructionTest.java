0d3f6ead77b048ddf542a64b7e567e11b1d62890
Juan Carlos Robles <carlos.jcr@hotmail.com>
2024-06-04 20:29:01 +0200

    add and vector instruction

---
+++ src/test/java/com/chip8/model/core/instruction/AndVectorInstructionTest.java
@@ -0,0 +1,69 @@
+package com.chip8.model.core.instruction;
+
+import com.chip8.api.core.register.VectorRegister;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HexFormat;
+
+@ExtendWith(MockitoExtension.class)
+class AndVectorInstructionTest {
+
+    @Mock
+    private VectorRegister vectorXRegister;
+
+    @Mock
+    private VectorRegister vectorYRegister;
+
+    private AndVectorInstruction orVectorInstruction;
+
+    @BeforeEach
+    void setUp() {
+        this.orVectorInstruction = new AndVectorInstruction(this.vectorXRegister, this.vectorYRegister);
+    }
+
+    @Test
+    void isExecutable_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.orVectorInstruction.isExecutable(null));
+    }
+
+    @Test
+    void isExecutable_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.orVectorInstruction.isExecutable(""));
+    }
+
+    @Test
+    void isExecutable_inputCommand_returnFalse_test() {
+        Assertions.assertFalse(this.orVectorInstruction.isExecutable("8000"));
+    }
+
+    @Test
+    void isExecutable_inputCommand_returnTrue_test() {
+        Assertions.assertTrue(this.orVectorInstruction.isExecutable("8FF2"));
+    }
+
+    @Test
+    void run_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.orVectorInstruction.run(null));
+    }
+
+    @Test
+    void run_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.orVectorInstruction.run(""));
+    }
+
+    @Test
+    void run_test() {
+        Mockito.when(this.vectorXRegister.getVRegister(HexFormat.fromHexDigits("A"))).thenReturn(1);
+        Mockito.when(this.vectorYRegister.getVRegister(HexFormat.fromHexDigits("F"))).thenReturn(2);
+        this.orVectorInstruction.run("8AF2");
+        Mockito.verify(this.vectorXRegister, Mockito.times(1))
+                .setVRegister(HexFormat.fromHexDigits("A"), 0);
+    }
+
+}
