ba2e86ae38b8a8e0c1bb56dbd4c606494d193555
Juan Carlos Robles <carlos.jcr@hotmail.com>
2024-06-05 20:05:32 +0200

    add random instruction

--- src/main/java/com/chip8/configure/AppConfigure.java
+++ src/main/java/com/chip8/configure/AppConfigure.java
@@ -4,17 +4,23 @@ import com.chip8.model.core.register.VectorRegisterHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Random;
+
 @Configuration
 public class AppConfigure {
 
     @Bean("vectorXRegister")
-    public VectorRegisterHandler vectorXRegister(){
+    public VectorRegisterHandler vectorXRegister() {
         return new VectorRegisterHandler();
     }
 
     @Bean("vectorYRegister")
-    public VectorRegisterHandler vectorYRegister(){
+    public VectorRegisterHandler vectorYRegister() {
         return new VectorRegisterHandler();
     }
 
+    @Bean
+    public Random random() {
+        return new Random();
+    }
 }
---
+++ src/test/java/com/chip8/model/core/instruction/RandomInstructionTest.java
@@ -0,0 +1,64 @@
+package com.chip8.model.core.instruction;
+
+import com.chip8.api.core.register.VectorRegister;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.HexFormat;
+import java.util.Random;
+
+@ExtendWith(MockitoExtension.class)
+class RandomInstructionTest {
+
+    @Mock
+    private VectorRegister vectorXRegister;
+
+    @Mock
+    private Random random;
+
+    @InjectMocks
+    private RandomInstruction randomInstruction;
+
+    @Test
+    void isExecutable_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.randomInstruction.isExecutable(null));
+    }
+
+    @Test
+    void isExecutable_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.randomInstruction.isExecutable(""));
+    }
+
+    @Test
+    void isExecutable_inputCommand_returnFalse_test() {
+        Assertions.assertFalse(this.randomInstruction.isExecutable("0000"));
+    }
+
+    @Test
+    void isExecutable_inputCommand_returnTrue_test() {
+        Assertions.assertTrue(this.randomInstruction.isExecutable("CFFF"));
+    }
+
+    @Test
+    void run_inputNull_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.randomInstruction.run(null));
+    }
+
+    @Test
+    void run_inputEmpty_returnException_test() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> this.randomInstruction.run(""));
+    }
+
+    @Test
+    void run_test() {
+        Mockito.when(this.random.nextInt()).thenReturn(255);
+        this.randomInstruction.run("C1FF");
+        Mockito.verify(this.vectorXRegister, Mockito.times(1)).setVRegister(1, HexFormat.fromHexDigits("FF"));
+    }
+
+}
